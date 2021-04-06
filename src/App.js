a7f47da7220e9fb526df43e138e13542df9db556
Alex Tristram <allex.tristram.gmail.com>
2021-04-06 16:47:23 +0100

    added first components to my app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import SubHeader from "./components/SubHeader";
+import SubNavbar from "./components/SubNavbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <SubHeader />
+      <Router>
+        <ArticleList path="/articles" />
+      </Router>
+      <SubNavbar />
+      <Navbar />
     </div>
   );
 }
